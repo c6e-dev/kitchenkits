ac0bcec5f2f320b5ab47570b31bd348c9dd58e46
0deusexmachina <lanznavarro20@gmail.com>
2019-01-09 07:40:34 +0800

    added recipe browsing page, fixed linking and updated home styles, deleted user guide

--- application/views/customer/cs_region.php
+++ application/views/customer/cs_region.php
@@ -18,33 +18,33 @@
 				<div id= "trigger-a" class="hidden">
 					<div id="fill_height" class="row">
 							<div id="France" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1 class="odd-b">France</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2 class="odd-b">France</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/France.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Greece" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1>Greece</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2>Greece</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/Greece.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Italy" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1 class="odd-b">Italy</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2 class="odd-b">Italy</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/Italy.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Mexico" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1>Mexico</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2>Mexico</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/Mexico.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Spain" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1 class="odd-b">Spain</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2 class="odd-b">Spain</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/Spain.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="States" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1>USA</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2>USA</h2>
 								<img src="<?php echo base_url('/assets/img/countries/west/US.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 					</div>
@@ -58,32 +58,32 @@
 				<div id="trigger-b" class="hidden">
 					<div id="fill_height" class="row">
 							<div id="China" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1 class="odd-b">China</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2 class="odd-b">China</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/China.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="India" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1>India</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2>India</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/India.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Japan" class="col-lg-2 region">
-								<a href="#"><span></span></a>
-								<h1 class="odd-b">Japan</h1>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
+								<h2 class="odd-b">Japan</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/Japan.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Phil" class="col-lg-2 region">
-								<a href="#"><span></span></a>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
 								<h2>Philippines</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/Phil.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Korea" class="col-lg-2 region">
-								<a href="#"><span></span></a>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
 								<h2 class="odd-b">South Korea</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/SK.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
 							<div id="Thailand" class="col-lg-2 region">
-								<a href="#"><span></span></a>
+								<a href="<?php echo site_url('user/load_csrecipe');?>"><span></span></a>
 								<h2>Thailand</h2>
 								<img id="for-center" src="<?php echo base_url('/assets/img/countries/east/Thailand.jpg'); ?>" alt="" width="100px" height="100px;">
 							</div>
