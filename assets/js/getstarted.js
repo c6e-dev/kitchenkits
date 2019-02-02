7556fde122972bd5d960d17d6897cefa668ac306
0deusexmachina <lanznavarro20@gmail.com>
2019-02-02 23:28:14 +0800

    region view FINISHED + RESPONSIVE

--- assets/js/getstarted.js
+++ assets/js/getstarted.js
@@ -1,24 +1,33 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container-fluid');
-
+const contain = document.querySelector('.placeholder');
+const contai = document.querySelector('.placeholder2');
+const west = document.querySelector('#trigger-a');
+const east = document.querySelector('#trigger-b');
 left.addEventListener('click', () =>{
 	container.classList.add('hover-left');
+	if (contain.classList.contains('hidden')) {
+		contain.classList.remove('hidden');
+	}
 });
-
 left.addEventListener('mouseleave', () =>{
 	container.classList.remove('hover-left');
 });
-
 right.addEventListener('click', () =>{
 	container.classList.add('hover-right');
+	if (contai.classList.contains('hidden')) {
+		contai.classList.remove('hidden');
+	}
 });
-
 right.addEventListener('mouseleave', () =>{
 	container.classList.remove('hover-right');
 });
-switch_b.addEventListener('mouseleave', () =>{
-	container.classList.add('switch-left');
+west.addEventListener('mouseleave', () =>{
+	contain.classList.add('hidden');
+});
+east.addEventListener('mouseleave', () =>{
+	contai.classList.add('hidden');
 });
 /**
 function unhide(divID1, divID2) {
